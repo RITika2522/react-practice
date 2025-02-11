acf55995d5f61bd6af675f56031eee5ac9f98659
RITika2522 <ritikachandak2502@gmail.com>
2025-02-11 09:51:58 +0530

    New react pratice

---
+++ fristproject/src/App.jsx
@@ -0,0 +1,14 @@
+import FirstApp from "./first.jsx"
+function App() {
+  
+  return (
+    <>
+      
+      <h1>Vite + React</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda doloremque quia temporibus, molestiae facilis dicta tempore suscipit consectetur minima! Dolore cupiditate ratione vitae nisi, facilis soluta tempore expedita dignissimos tempora.</p>
+      <FirstApp />
+    </>
+  )
+}
+
+export default App
---
+++ fristproject/src/first.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function FirstApp() {
+  return (
+    <div>
+        <h1>Hello world</h1>
+    </div>
+  )
+}
+
+export default FirstApp
