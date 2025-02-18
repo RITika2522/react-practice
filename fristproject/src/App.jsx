cf3a28532ed8bbd79703f8d268e7556da49e8ef8
RITika2522 <ritikachandak2502@gmail.com>
2025-02-19 02:17:09 +0530

    app.jsx modified

--- fristproject/src/App.jsx
+++ fristproject/src/App.jsx
@@ -7,7 +7,7 @@ function App() {
       <h1>Vite + React</h1>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda doloremque quia temporibus, molestiae facilis dicta tempore suscipit consectetur minima! Dolore cupiditate<br/> ratione vitae nisi, facilis soluta tempore expedita dignissimos tempora.</p>
       <FirstApp sal="10000" desig="Software Engineer" com="Google"/>
-      <FirstApp sal="20000" desig="Software Developer" com="Facebook"/>
+      <FirstApp sal="20000" com="Facebook"/>
       <FirstApp sal="30000" desig="Software Tester" com="Amazon"/>
     </>
   )
